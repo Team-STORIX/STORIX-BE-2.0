97f7668e0c272e11b053b13167f71e6174538818
Immmii <leesa7118@gmail.com>
2026-03-13 13:07:12 +0900

    Refactor: [Domain/Favorite] 작가 기능 삭제

--- STORIX-Domain/src/main/java/com/storix/domain/domains/favorite/service/FavoriteService.java
+++ STORIX-Domain/src/main/java/com/storix/domain/domains/favorite/service/FavoriteService.java
@@ -1,12 +1,7 @@
 package com.storix.domain.domains.favorite.service;
 
-import com.storix.domain.domains.favorite.adaptor.FavoriteArtistAdaptor;
 import com.storix.domain.domains.favorite.adaptor.FavoriteWorksAdaptor;
-import com.storix.domain.domains.user.adaptor.UserAdaptor;
-import com.storix.domain.domains.user.domain.Role;
-import com.storix.domain.domains.favorite.exception.DuplicateFavoriteArtistRequestException;
 import com.storix.domain.domains.favorite.exception.DuplicateFavoriteWorksRequestException;
-import com.storix.domain.domains.favorite.exception.FavoriteArtistNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,8 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class FavoriteService {
 
     private final FavoriteWorksAdaptor favoriteWorksAdaptor;
-    private final FavoriteArtistAdaptor favoriteArtistAdaptor;
-    private final UserAdaptor userAdaptor;
 
     // [작품] 관심 작품 관련
     @Transactional(readOnly = true)
@@ -38,40 +31,6 @@ public class FavoriteService {
     @Transactional
     public void deleteFavoriteWorks(Long userId, Long worksId) {
         favoriteWorksAdaptor.deleteSingleFavoriteWorks(userId, worksId);
-    }
-
-    // [작품] 관심 작가 관련
-    @Transactional(readOnly = true)
-    public boolean isFavoriteArtist(Long userId, Long artistId) {
-
-        // 작가 계정 확인
-        Role role = userAdaptor.findUserRoleByUserId(artistId);
-        if (role.equals(Role.READER)) {
-            throw FavoriteArtistNotFoundException.EXCEPTION;
-        }
-
-        return favoriteArtistAdaptor.isFavoriteArtistAlreadySelected(userId, artistId);
-    }
-
-    @Transactional
-    public void saveFavoriteArtist(Long userId, Long artistId) {
-
-        // 작가 계정 확인
-        Role role = userAdaptor.findUserRoleByUserId(artistId);
-        if (role.equals(Role.READER)) {
-            throw FavoriteArtistNotFoundException.EXCEPTION;
-        }
-
-        if (favoriteArtistAdaptor.isFavoriteArtistAlreadySelected(userId, artistId)) {
-            throw DuplicateFavoriteArtistRequestException.EXCEPTION;
-        }
-
-        favoriteArtistAdaptor.saveSingleFavoriteArtist(userId, artistId);
-    }
-
-    @Transactional
-    public void deleteFavoriteArtist(Long userId, Long artistId) {
-        favoriteArtistAdaptor.deleteSingleFavoriteArtist(userId, artistId);
     }
 
 }
