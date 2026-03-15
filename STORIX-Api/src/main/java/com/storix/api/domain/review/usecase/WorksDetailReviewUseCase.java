b519e5396380adc91fb39b8f3cb484bc93612cfd
Immmii <leesa7118@gmail.com>
2026-03-15 20:23:30 +0900

    Refactor: 둘러보기 기능 삭제로 인한 권한 재세팅

--- STORIX-Api/src/main/java/com/storix/api/domain/review/usecase/WorksDetailReviewUseCase.java
+++ STORIX-Api/src/main/java/com/storix/api/domain/review/usecase/WorksDetailReviewUseCase.java
@@ -20,32 +20,19 @@ public class WorksDetailReviewUseCase {
 
     // 내 리뷰 조회
     public CustomResponse<SliceReviewInfo> getMyReview(AuthUserDetails authUserDetails, Long worksId) {
-
-        Long userId = (authUserDetails != null) ? authUserDetails.getUserId() : null;
-
-        if (userId == null) {
-            return CustomResponse.onSuccess(SuccessCode.SUCCESS, null);
-        }
-
-        SliceReviewInfo result = reviewService.findMyReview(userId, worksId);
+        SliceReviewInfo result = reviewService.findMyReview(authUserDetails.getUserId(), worksId);
         return CustomResponse.onSuccess(SuccessCode.WORKS_DETAIL_MY_REVIEW_LOAD_SUCCESS, result);
     }
 
     // 다른 유저 리뷰 전체 조회
     public CustomResponse<Slice<SliceReviewInfoWithProfile>> getOtherReview(AuthUserDetails authUserDetails, Long worksId, Pageable pageable) {
-
-        Long userId = (authUserDetails != null) ? authUserDetails.getUserId() : null;
-
-        Slice<SliceReviewInfoWithProfile> result = reviewService.findAllReviewWithoutMine(userId, worksId, pageable);
+        Slice<SliceReviewInfoWithProfile> result = reviewService.findAllReviewWithoutMine(authUserDetails.getUserId(), worksId, pageable);
         return CustomResponse.onSuccess(SuccessCode.WORKS_DETAIL_OTHER_REVIEW_LOAD_SUCCESS, result);
     }
 
     // 리뷰 단건 조회
     public CustomResponse<DetailedReviewInfoWithProfile> getReviewDetail(AuthUserDetails authUserDetails, Long reviewId) {
-
-        Long userId = (authUserDetails != null) ? authUserDetails.getUserId() : null;
-
-        DetailedReviewInfoWithProfile result = reviewService.findReviewDetail(userId, reviewId);
+        DetailedReviewInfoWithProfile result = reviewService.findReviewDetail(authUserDetails.getUserId(), reviewId);
         return CustomResponse.onSuccess(SuccessCode.WORKS_DETAIL_REVIEW_DETAIL_LOAD_SUCCESS, result);
     }
 
