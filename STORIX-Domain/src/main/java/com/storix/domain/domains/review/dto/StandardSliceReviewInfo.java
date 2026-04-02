ed60476e25ebc2775e2176dbb7b3a0803f6c7761
Immmii <leesa7118@gmail.com>
2026-04-02 23:42:19 +0900

    Refactor: 다른 유저 리뷰 리스트 - 미리보기 : 평점 데이터 추가

--- STORIX-Domain/src/main/java/com/storix/domain/domains/review/dto/StandardSliceReviewInfo.java
+++ STORIX-Domain/src/main/java/com/storix/domain/domains/review/dto/StandardSliceReviewInfo.java
@@ -1,19 +1,24 @@
 package com.storix.domain.domains.review.dto;
 
+import com.storix.domain.domains.plus.domain.Rating;
 import com.storix.domain.domains.plus.dto.SliceReviewInfo;
+import lombok.Builder;
 
+@Builder
 public record StandardSliceReviewInfo(
         Long reviewId,
         boolean isSpoiler,
         String spoilerScript,
-        String content
+        String content,
+        Rating rating
 ) {
     public static StandardSliceReviewInfo from(SliceReviewInfo review) {
-        return new StandardSliceReviewInfo(
-                review.reviewId(),
-                review.isSpoiler(),
-                review.spoilerScript(),
-                review.content()
-        );
+        return StandardSliceReviewInfo.builder()
+                .reviewId(review.reviewId())
+                .isSpoiler(review.isSpoiler())
+                .spoilerScript(review.spoilerScript())
+                .content(review.content())
+                .rating(review.rating())
+                .build();
     }
 }
