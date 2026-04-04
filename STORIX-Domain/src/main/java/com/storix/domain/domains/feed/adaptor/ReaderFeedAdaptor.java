be4dd7bd2f8e0509eb6c89cc41f7d95c1dd2de0c
Immmii <leesa7118@gmail.com>
2026-04-04 18:06:53 +0900

    Refactor: 토픽룸/오늘의 피드 캐싱

--- STORIX-Domain/src/main/java/com/storix/domain/domains/feed/adaptor/ReaderFeedAdaptor.java
+++ STORIX-Domain/src/main/java/com/storix/domain/domains/feed/adaptor/ReaderFeedAdaptor.java
@@ -16,6 +16,7 @@ import com.storix.domain.domains.feed.exception.BoardReplyNotFoundException;
 import com.storix.domain.domains.feed.exception.InvalidBoardRequestException;
 import com.storix.domain.domains.user.exception.auth.ForbiddenApproachException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -240,12 +241,14 @@ public class ReaderFeedAdaptor {
     }
 
     // 오늘의 피드
+    @Cacheable(cacheNames = "trendingFeed", key = "'top3'", cacheManager = "trendingCacheManager")
     public List<StandardReaderBoardInfo> findTop3TrendingFeed(LocalDateTime threshold) {
         Pageable pageable = PageRequest.of(0, 3);
 
         return readerBoardRepository.findTop3TrendingFeed(threshold, pageable);
     }
 
+    @Cacheable(cacheNames = "steadyTrendingFeed", key = "#excludeIds.hashCode() + '_' + #limit", cacheManager = "trendingCacheManager")
     public List<StandardReaderBoardInfo> findSteadyTrendingFeedNotToday(List<Long> excludeIds, int limit, LocalDateTime threshold) {
         Pageable pageable = PageRequest.of(0, limit);
 
