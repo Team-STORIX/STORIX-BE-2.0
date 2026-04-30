37fad3f7a717619760035a1cac7d9ab06662a426
Immmii <leesa7118@gmail.com>
2026-05-01 00:02:51 +0900

    Refactor: [web/native 지원] 토큰 response DTO

--- STORIX-Api/src/main/java/com/storix/api/domain/user/controller/dto/AuthorizationResponse.java
+++ STORIX-Api/src/main/java/com/storix/api/domain/user/controller/dto/AuthorizationResponse.java
@@ -1,6 +1,17 @@
 package com.storix.api.domain.user.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record AuthorizationResponse(
-        String accessToken
+        String accessToken,
+        String refreshToken
 ) {
+    public static AuthorizationResponse webRefresh(String accessToken) {
+        return new AuthorizationResponse(accessToken, null);
+    }
+
+    public static AuthorizationResponse nativeRefresh(String accessToken, String refreshToken) {
+        return new AuthorizationResponse(accessToken, refreshToken);
+    }
 }
--- STORIX-Api/src/main/java/com/storix/api/domain/user/controller/dto/ReaderLoginResponse.java
+++ STORIX-Api/src/main/java/com/storix/api/domain/user/controller/dto/ReaderLoginResponse.java
@@ -1,6 +1,17 @@
 package com.storix.api.domain.user.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ReaderLoginResponse(
-        String accessToken
+        String accessToken,
+        String refreshToken
 ) {
+    public static ReaderLoginResponse webLogin(String accessToken) {
+        return new ReaderLoginResponse(accessToken, null);
+    }
+
+    public static ReaderLoginResponse nativeLogin(String accessToken, String refreshToken) {
+        return new ReaderLoginResponse(accessToken, refreshToken);
+    }
 }
