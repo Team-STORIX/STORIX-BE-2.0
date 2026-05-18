44e9d2859ad0037f518477ced6e36240bd8c49c7
Immmii <leesa7118@gmail.com>
2026-05-19 01:22:11 +0900

    Refactor: [Auth] 회원탈퇴 시 소셜 서비스 unlink 기능 try-catch 처리

--- STORIX-Api/src/main/java/com/storix/api/domain/user/usecase/WithDrawUseCase.java
+++ STORIX-Api/src/main/java/com/storix/api/domain/user/usecase/WithDrawUseCase.java
@@ -8,8 +8,10 @@ import com.storix.api.domain.user.helper.CookieHelper;
 import com.storix.common.payload.CustomResponse;
 import com.storix.common.code.SuccessCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 
+@Slf4j
 @UseCase
 @RequiredArgsConstructor
 public class WithDrawUseCase {
@@ -22,15 +24,21 @@ public class WithDrawUseCase {
     public ResponseEntity<CustomResponse<Void>> execute(Long userId) {
 
         // 1. OAuth 연결 해제
-        // TODO: 현재 외부 api 호출 실패 시 feignException 터짐 -> 회원 탈퇴 X -> best-effort + 재시도 워커 처리 고려중
         OAuthInfo oauthInfo = authService.findOAuthInfoByUserId(userId);
-        switch (oauthInfo.getProvider()) {
-            case KAKAO -> oauthHelper.unlinkKakaoUser(oauthInfo.getOid());
-            case NAVER -> oauthHelper.unlinkNaverUser(oauthInfo.getOid());
-            case SLACK -> {} // admin 만 해당 (연결 해제 불필요)
-            // TODO: Apple 은 refresh_token 저장 후 unlinkAppleUser 호출 필요
-            case APPLE -> {}
+        try {
+            switch (oauthInfo.getProvider()) {
+                case KAKAO -> oauthHelper.unlinkKakaoUser(oauthInfo.getOid());
+                case NAVER -> oauthHelper.unlinkNaverUser(oauthInfo.getOid());
+                case SLACK -> {} // admin 만 해당 (연결 해제 불필요)
+                // TODO: Apple 은 refresh_token 저장 후 unlinkAppleUser 호출 필요
+                case APPLE -> {}
+            }
+        } catch (Exception e) {
+            log.warn("OAuth unlink failed; continuing withdraw. userId={}, provider={}",
+                    userId, oauthInfo.getProvider(), e);
+            // TODO: best-effort + 재시도 워커 처리 고려중
         }
+
 
         // 2. 유저 탈퇴 처리 (RefreshToken / 관심작품 / 서재 삭제 + 푸시 알림 발송 대상 제외)
         authService.withDrawUser(userId);
