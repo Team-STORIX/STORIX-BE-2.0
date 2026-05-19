61e68607f92883b7a0124dda55143743c671e598
Immmii <leesa7118@gmail.com>
2026-05-19 16:57:01 +0900

    Refactor: NotificationSetting 관련 DTO 시그니처 변경

---
+++ STORIX-Domain/src/main/java/com/storix/domain/domains/notification/dto/UpdateNotificationSettingCommand.java
@@ -0,0 +1,13 @@
+package com.storix.domain.domains.notification.dto;
+
+public record UpdateNotificationSettingCommand(
+        Boolean likeFeedEnabled,
+        Boolean likeReviewEnabled,
+        Boolean likeCommentEnabled,
+        Boolean commentOnFeedEnabled,
+        Boolean replyOnCommentEnabled,
+        Boolean todayFeedEnabled,
+        Boolean hotTopicRoomEnabled,
+        Boolean marketingEnabled
+) {
+}
