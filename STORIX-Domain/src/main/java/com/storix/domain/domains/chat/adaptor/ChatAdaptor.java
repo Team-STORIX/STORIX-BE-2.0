a67e35bbf88c2f5cca1db6efcabb39c66254672f
Hoyoung027 <hybyun0207@gmail.com>
2026-05-31 00:55:52 +0900

    feat : 채팅방 입장시각 반환 추가

--- STORIX-Domain/src/main/java/com/storix/domain/domains/chat/adaptor/ChatAdaptor.java
+++ STORIX-Domain/src/main/java/com/storix/domain/domains/chat/adaptor/ChatAdaptor.java
@@ -1,8 +1,11 @@
 package com.storix.domain.domains.chat.adaptor;
 
 import com.storix.domain.domains.chat.domain.ChatMessage;
+import com.storix.domain.domains.chat.dto.ChatMessageResponseDto;
 import com.storix.domain.domains.chat.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,5 +16,9 @@ public class ChatAdaptor {
 
     public ChatMessage saveMessage(ChatMessage message) {
         return chatRepository.save(message);
+    }
+
+    public Slice<ChatMessageResponseDto> loadMessages(Long roomId, Pageable pageable) {
+        return chatRepository.findAllByRoomIdOrderByCreatedAtDesc(roomId, pageable);
     }
 }
